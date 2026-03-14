9e2aa5e11a64b5897dab84e324a81c940feb7550
NARAYANI105 <953625104105@ritrjpm.ac.in>
2026-03-14 18:32:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 function login(){
 
-let id=document.getElementById("id").value;
-let pass=document.getElementById("password").value;
+let id = document.getElementById("id").value;
+let pass = document.getElementById("password").value;
 
-if(id.endsWith("@ritrjpm.ac.in") && pass==="RIT")
-{
-window.location.href="side.html";
+if(id.endsWith("@ritrjpm.ac.in") && pass === "RIT"){
+    window.location.href = "side.html";
 }
-else
-{
-alert("Invalid Login");
+else{
+    alert("Invalid Login");
 }
 
 }
